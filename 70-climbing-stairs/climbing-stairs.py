713aec7d66a96210ef9ca0cf8672381c03e02e1d
Jordan Lloyd <10lloydj@gmail.com>
2025-06-14 17:17:30 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.49%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        first = 1
+        second = 2
+        current = 2
+        if n < 3:
+            return n
+        for i in range(3, (n+1)):
+            current = first + second
+            first = second
+            second = current
+        return current
