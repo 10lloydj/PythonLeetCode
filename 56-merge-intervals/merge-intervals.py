4b5bf75eb3aadcdc8cf3f464117eb2842a3828d2
Jordan Lloyd <10lloydj@gmail.com>
2025-08-31 23:04:46 +0100

    Time: 5 ms (81.74%) | Memory: 20.9 MB (63.15%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,10 +1,5 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) == 0:
-            return []
-        if len(intervals) == 1:
-            return intervals
-
         intervals.sort(key=lambda x: x[0])
         merged = [intervals[0]]
         # since its sorted, I know the start can never be less than the previous so I need to focus on the last values in comparisons
@@ -14,11 +9,8 @@ class Solution:
             if current[0] > last[1]:
                 merged.append(current)
                 continue
-            if current[1] <= last[1]:
-                continue
-            if current[0] <= last[1]:
+            else:
                 # edit last to have a new end
                 merged[-1][1] = max(merged[-1][1], current[1])
                 continue
-            merged.append(current)
         return merged
