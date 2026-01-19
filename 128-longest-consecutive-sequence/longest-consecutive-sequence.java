b21dde5575f048861ffaf63965121cb178f016b9
Jordan Lloyd <10lloydj@gmail.com>
2026-01-19 21:22:17 +0000

    Time: 30 ms (77.22%) | Memory: 96 MB (22.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,6 +1,7 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
+        if (nums.length == 1) return 1;
         // hashset
         HashSet<Integer> set = new HashSet<Integer>();
         for(int x: nums) {
@@ -10,23 +11,19 @@ class Solution {
         // check if current has a lower value, then go up
         for(int a : set) {
             int current = a;
-            int count = 0;
+            int count = 1;
             // iterates to the 'lowest' value
             if (set.contains(current - 1)) {
-                continue;
-            }
-
-            if (!set.contains(current + 1)) {
                 continue;
             }
 
             while (set.contains(current + 1)) {
                 count++;
                 current++;
-                max = Math.max(count, max);
             }
+            max = Math.max(count, max);
         }
 
-        return max + 1;
+        return max;
     }
 }
