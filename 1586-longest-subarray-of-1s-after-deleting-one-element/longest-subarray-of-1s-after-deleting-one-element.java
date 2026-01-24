dd320e42aaebf52d75433b2842924de16f306dcd
Jordan Lloyd <10lloydj@gmail.com>
2026-01-24 13:13:30 +0000

    Time: 3 ms (86.75%) | Memory: 65.4 MB (75.96%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,29 +1,21 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int best = 0, l = 0;
-        int delLeft = 1;
+        int left = 0;
+        int zeros = 0;
+        int bestWindow = 0;
 
-        for(int r = 0; r < nums.length; r++) {
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) zeros++;
 
-            if(nums[r] == 0) {
-                delLeft--;
+            while (zeros > 1) {
+                if (nums[left] == 0) zeros--;
+                left++;
             }
 
-            while (delLeft < 0) {
-                if (nums[l] == 0) {
-                    delLeft++;
-                }
-                l++;
-            }
-
-            // if delLeft = 1 and best < length. best is still the same
-            if (delLeft == 1 && (r - l + 1) < nums.length) {
-                best = Math.max(best, r - l + 1);
-            } else {
-                // if delLeft = 0 best - 1
-                best = Math.max(best, r - l);
-            }
+            bestWindow = Math.max(bestWindow, right - left + 1);
         }
-        return best;
+
+        // must delete exactly one element
+        return bestWindow - 1;
     }
 }
